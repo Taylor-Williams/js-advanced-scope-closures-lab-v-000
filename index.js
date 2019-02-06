8ea5d05dfaf65af883a705aba1527dedf3d33932
Taylor Williams <taylor@thewilliams.net>
2019-02-06 08:06:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function produceDrivingRange(blockRange){
+  return function(firstBlock, secondBlock){
+    let range = Math.abs(parseInt(firstBlock, 10) - parseInt(secondBlock, 10))
+    range = blockRange - range
+    range >= 0 ? range = `within range by ${range}` : range = `${-1 * range} blocks out of range`
+    return range
+  }
+}
+function produceTipCalculator(percentage){
+  return function(cost){
+    return percentage * cost
+  }
+}
+function createDriver() {
+  let driverId = 0
+  return class{
+    constructor(name){
+      this.name = name
+      this.id = driverId++
+    }
+  }
+}
