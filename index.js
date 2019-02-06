3fdd06674d9057ed2dd56bdac0c95225f391f687
Taylor Williams <taylor@thewilliams.net>
2019-02-06 08:06:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ function produceTipCalculator(percentage){
   }
 }
 function createDriver() {
+  let driverId = 0
   return class{
     constructor(name){
       this.name = name
+      this.id = driverId++
     }
   }
 }
