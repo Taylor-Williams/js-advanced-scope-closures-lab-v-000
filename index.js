844ec2fba34ea7212bbf98a35ed73006d0f39ec6
Taylor Williams <taylor@thewilliams.net>
2019-02-06 07:59:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function produceDrivingRange(blockRange){
   return function(firstBlock, secondBlock){
     let range = Math.abs(parseInt(firstBlock, 10) - parseInt(secondBlock, 10))
-    range > this.blockRange ? return `within range by ${range}` : return `${range} blocks out of range`
+    range > this.blockRange ? range = `within range by ${range}` : range = `${range} blocks out of range`
+    return range
   }
 }
